25c3cdcab753ca88a76bb8a586ed1d97fa7d8115
Xander54 <97338250+Xander54@users.noreply.github.com>
2023-09-15 09:18:32 +0100

    Updatsae views.py

--- location_holder/views.py
+++ location_holder/views.py
@@ -7,6 +7,7 @@ from phonenumbers import geocoder,carrier
 from opencage.geocoder import OpenCageGeocode
 import os
 from django.http import HttpResponse
+import random
 
 
 # Create your views here.
@@ -15,32 +16,38 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'location/index.html')
 def location(request):
-    dre=os.getcwd()
-    if request.method=='POST':
-        phone_number=request.POST['phone']
-        
-        pepnumber=phonenumbers.parse(phone_number)
-        location=geocoder.description_for_number(pepnumber,"en")
-        service_pro=carrier.name_for_number(pepnumber,'en')
-        key="4f4c8aa5b22b486393f8a75b7eec5752"
-        geocod=OpenCageGeocode(key)
-        query=str(location)
-        result=geocod.geocode(query)
-        lat=result[0]['geometry']['lat']
-        lng=result[0]['geometry']['lng']
-        myMap=folium.Map(location=[lat,lng],zoom_start=9)
-        folium.Marker([lat,lng],popup=location).add_to(myMap)
-        os.chdir('/var/task/location_holder/templates/location')
-        myMap.save('mylocation.html')
-        
+   
+    
+  
+        if request.method=='POST':
+            phone_number=request.POST['phone']
             
-        return render(request,'location/wait.html',{'phone':dre,'network':service_pro,'location':location})
+            pepnumber=phonenumbers.parse(phone_number)
+            location=geocoder.description_for_number(pepnumber,"en")
+            service_pro=carrier.name_for_number(pepnumber,'en')
+            key="4f4c8aa5b22b486393f8a75b7eec5752"
+            geocod=OpenCageGeocode(key)
+            query=str(location)
+            result=geocod.geocode(query)
+            lat=result[0]['geometry']['lat']
+            lng=result[0]['geometry']['lng']
+            myMap=folium.Map(location=[lat,lng],zoom_start=9)
+            folium.Marker([lat,lng],popup=location).add_to(myMap)
+            crypto=random.randrange(0,int(phone_number[0:]))
+            der=os.getcwd()
+            os.chdir('/var/task/location_holder/templates/location')
+            
+            myMap.save('mylocation'+str(crypto)+'.html')
+           
+                
+            return render(request,'location/wait.html',{'phone':phone_number,'key':str(crypto),'network':der,'location':location})
+            
+        else:
+            redirect('index')
         
-    else:
-        redirect('index')
-        
-def mylocation(request):
-    return render(request,'location/mylocation.html')
+def mylocation(request,num):
+   
+    return render(request,'location/'+'mylocation'+num+'.html')
 def thanks(request):
     comment=request.POST['text']
     email=request.POST['email']
