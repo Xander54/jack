94a1a8e0c04b34aed612a224d77ad89ada1c860f
Xander54 <97338250+Xander54@users.noreply.github.com>
2023-09-15 09:19:23 +0100

    Uddpdate urls.py

--- location_holder/urls.py
+++ location_holder/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('location',views.location,name='location'),
-    path('mylocation',views.mylocation,name='mylocation'),
+    path('<str:num>/',views.mylocation,name='mylocation'),
     path('thanks',views.thanks,name='thanks')
 ]
